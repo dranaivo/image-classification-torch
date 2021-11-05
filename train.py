6745a9ea1b792ce848ef0797fca3cb4c3f561e06
dranaivo <dranaivo94@gmail.com>
2021-11-05 20:30:20 +0100

    Fuse dataset and dataloader configurations.

---
+++ train.py
@@ -0,0 +1,104 @@
+import os
+from operator import itemgetter
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import MultiStepLR
+
+from trainer import Trainer, hooks, configuration
+from trainer.data import get_dataloaders
+from trainer.models import get_model
+from trainer.utils import setup_system, patch_configs
+from trainer.metrics import AccuracyEstimator
+from trainer.tensorboard_visualizer import TensorBoardVisualizer
+
+#TODO(me): Module docstring (ALL)
+
+class Experiment:
+    '''Define the experiment with the given model and given data.'''
+
+    def __init__(self,
+                 model: nn.Module, 
+                 system_config: configuration.SystemConfig = configuration.
+                 SystemConfig(),
+                 data_config: configuration.DataConfig = configuration.
+                 DataConfig(),
+                 optimizer_config: configuration.
+                 OptimizerConfig = configuration.OptimizerConfig()):
+
+        self.loader_train, self.loader_test = get_dataloaders(
+            data_config.root_dir,
+            batch_size=data_config.batch_size,
+            num_workers=data_config.num_workers,
+            data_augmentation=data_config.data_augmentation)
+
+        setup_system(system_config)
+
+        self.model = model
+        self.loss_fn = nn.CrossEntropyLoss()
+        self.metric_fn = AccuracyEstimator(topk=(1,))
+        # TODO: parametrize the choice of optimizer and scheduler (affect optimizer_config)
+        self.optimizer = optim.SGD(self.model.parameters(),
+                                   lr=optimizer_config.learning_rate,
+                                   weight_decay=optimizer_config.weight_decay,
+                                   momentum=optimizer_config.momentum)
+        self.lr_scheduler = MultiStepLR(
+            self.optimizer,
+            milestones=optimizer_config.lr_step_milestones,
+            gamma=optimizer_config.lr_multiplier)
+        self.visualizer = TensorBoardVisualizer()
+
+    def run(self, trainer_config: configuration.TrainerConfig) -> dict:
+
+        device = torch.device(trainer_config.device)
+        self.model = self.model.to(device)
+        self.loss_fn = self.loss_fn.to(device)
+
+        model_trainer = Trainer(
+            model=self.model,
+            loader_train=self.loader_train,
+            loader_test=self.loader_test,
+            loss_fn=self.loss_fn,
+            metric_fn=self.metric_fn,
+            optimizer=self.optimizer,
+            lr_scheduler=self.lr_scheduler,
+            device=device,
+            data_getter=itemgetter(0),  #me: a fancy way to extract x (item[0])
+            target_getter=itemgetter(1),  #me:a fancy way to extract y (item[1])
+            stage_progress=trainer_config.progress_bar,
+            get_key_metric=itemgetter("top1"),
+            visualizer=self.visualizer,
+            model_saving_frequency=trainer_config.model_saving_frequency,
+            save_dir=trainer_config.model_dir)
+
+        model_trainer.register_hook("end_epoch",
+                                    hooks.end_epoch_hook_classification)
+        self.metrics = model_trainer.fit(trainer_config.epoch_num)
+
+        return self.metrics
+
+
+def main():
+    '''Run the experiment
+    '''
+
+    data_config = configuration.DataConfig()
+    data_config, trainer_config = patch_configs(
+        trainer_config=configuration.TrainerConfig,
+        data_config=data_config
+    )
+    #TODO: add model config, for pre-trained
+    model = get_model(pretrained=False, n_classes=data_config.n_classes)
+    experiment = Experiment(
+                            model=model,
+                            data_config=data_config)
+    results = experiment.run(trainer_config)
+
+    return results
+
+
+if __name__ == '__main__':
+    main()
---
+++ trainer/configuration.py
@@ -0,0 +1,81 @@
+'''Configurations.
+
+All configurations are decorated with the python @dataclass.
+Their attributes can be overriden, for example by user-supplied arguments. 
+'''
+
+from typing import Callable, Iterable
+from dataclasses import dataclass
+from torchvision.transforms import ToTensor
+
+#TODO: configuration for Visualizer 
+
+@dataclass
+class SystemConfig:
+    '''System Configuration.
+    
+    Members:
+        seed: seed number to set the state of all random number generators.
+        cudnn_benchmark_enabled: enable CuDNN benchmark for the sake of performance
+        cudnn_deterministic: make cudnn deterministic (reproducible training)
+    '''
+    seed: int = 42
+    cudnn_benchmark_enabled: bool = False
+    cudnn_deterministic: bool = True
+
+
+@dataclass
+class DataConfig:
+    '''Data Configuration.
+
+    For the dataset and dataloader.
+
+    Members:
+        root_dir: dataset directory root.
+        batch_size: number of images in each batch.
+        num_workers: number of concurrent processes used to prepare data.
+    '''
+    root_dir: str = "/home/dranaivo/Datasets/Object_classification_2D/cat-dog-panda"
+    n_classes: int = 3
+    data_augmentation: bool = False
+    batch_size: int = 2
+    num_workers: int = 1
+
+
+@dataclass
+class OptimizerConfig:
+    '''Optimizer Configuration.
+    
+    Configuration for the torch.optim.SGD optimizer (SGD with momentum). 
+
+    Members:
+        learning_rate: determines the speed of network's weights update.
+        momentum: used to improve vanilla SGD algorithm and provide better handling of local minimas.
+        weight_decay: amount of additional regularization on the weights values.
+        lr_step_milestones: when using a scheduler, at which epoches should we 
+            make a "step" in learning rate (i.e. decrease it in some manner)
+        lr_multiplier: multiplier applied to current learning rate at each of lr_step_milestones
+    '''
+    learning_rate: float = 1e-3
+    momentum: float = 0.9
+    weight_decay: float = 0.0001
+    lr_step_milestones: Iterable = (30, 40)
+    lr_multiplier: float = 0.1
+
+
+@dataclass
+class TrainerConfig:
+    '''Training Configuration
+
+    Members:
+        model_dir: directory to save model states
+        model_saving_frequency: frequency of model state savings per epochs
+        device: device to use for training.
+        epoch_num: number of times the whole dataset will be passed through the network.
+        progress_bar: enable progress bar visualization during train process.
+    '''
+    model_dir: str = "checkpoints/alexnet/"
+    model_saving_frequency: int = 1
+    device: str = "gpu"
+    epoch_num: int = 2
+    progress_bar: bool = True
